4daf35088404b77d6aa746c0073a4033f5d83aba
Miguel Emmanuel Paredes Dailisan <mepdailisan@addunet.ph>
2023-03-17 19:39:51 +0800

    added chakra ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/card";
 import { useEffect, useState } from "react";
+import { ChakraBaseProvider } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -13,11 +15,13 @@ function App() {
   }, []);
 
   return (
+    <ChakraProvider>
     <div className="container">
       {posts.map((e) => (
         <Card key={e.id} title={e.title} body={e.body} />
       ))}
     </div>
+    </ChakraProvider>
   );
 }
 
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./card.css";
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function card(props) {
   const handleClick = (e) => {
@@ -10,7 +11,8 @@ function card(props) {
       <div className="card">
         <h3>{props.title}</h3>
         <p>{props.body}</p>
-        <button onClick={handleClick}>click me</button>
+        {/* <button onClick={handleClick}>click me</button> */}
+        <Button colorScheme='green' onClick={handleClick}>click me</Button>
       </div>
     </>
   );
